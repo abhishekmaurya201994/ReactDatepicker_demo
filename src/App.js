c0e73b2bf2c86d29359cb3dbbe311ea26b919b7f
LEARNINGMATEMUM\abhishek.maurya <abhishek.maurya@learningmate.com>
2018-10-12 18:20:31 +0530

    Datepicker changes

--- src/App.js
+++ src/App.js
@@ -1,163 +1,26 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
-
+import DatePickerInput from './Component/Datepickerindex';
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      startDate: '',midDate:'',
-      endDate: '',
-      checkpoint: []
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleChange_endDate = this.handleChange_endDate.bind(this);
-    this.handle_midDate=this.handle_midDate.bind(this);
-    // this.diff = this.diff.bind(this);
-    this.diffTest = this.diffTest.bind(this);
-    // this.validateWeekendDate=this.validateWeekendDate(this);
-  }
-
-  handleChange(date) {
- 
-    this.setState({
-      startDate: date
-    });
-  }
-  handleChange_endDate(date) {
-   
-    this.setState({
-      endDate: date
-    })
-  }
-
-  handle_midDate(date){
-
-    this.setState({
-      midDate: date
-    })
-  }
-
   
-
-
-  isWeekday = (date) => {
-    const day = date.day()
-    return day !== 0 && day !== 6
-  }
-  diffTest() {
-    let checkpointInterval =3;
-    let self = this;
-    // let a=[];
-    var a=[];
-    if (this.state.startDate && this.state.midDate) {
-       if(this.state.startDate<this.state.midDate){
-      let date1 = new Date(this.state.startDate);
-      let date2 = new Date(this.state.midDate);
-
-      let timeDiff = Math.abs(date2.getTime() - date1.getTime());
-      let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-console.log(diffDays);
-      let interval = Math.round(diffDays / checkpointInterval);
-console.log("interval "+interval);
-      var tomorrow = new Date();
-
-      for (var i = 1; i <= checkpointInterval; i++) {
-        // if (i == 1) {
-          console.log(i);
-          let checkpointIntData = interval * i;
-          /*To do*/
-          let checkpointInputDate = date1;
-        
-       
-         
-          checkpointInputDate.setDate(new Date(this.state.startDate).getMonth() + checkpointIntData);
-          // console.log(checkpointInputDate);
-               console.log(checkpointInputDate.getDay());
-               checkpointInputDate=this.validateWeekendDate(checkpointInputDate);
-                  // a.push(checkpointInputDate);
-                  // checkpointInputDate=new Date(checkpointInputDate);
-                  a.push(new Date(checkpointInputDate).toLocaleDateString());
-                  this.setState({checkpoint:a});
-              
-
-       
-      }
-      console.log("data: "+a);
-      // console.log(a);
-    }
-    else{
-      a=[];
-      alert("Start date should be less than mid date");
-      this.setState({checkpoint:a});
-    }
-    }
-    else {
-      alert("without state");
-    }
   }
 
-  validateWeekendDate(checkpointInputDate) {
-    checkpointInputDate=new Date(checkpointInputDate);
-               if(checkpointInputDate.getDay()==6 )
-               {
-                 console.log("get days");
-                checkpointInputDate.setDate(new Date(checkpointInputDate).getDate()-1);
-                console.log(checkpointInputDate);
-               }
-               if(checkpointInputDate.getDay()==0)
-               {
-                console.log("get days ++");
-                checkpointInputDate.setDate(new Date(checkpointInputDate).getDate()+1);
-               
-               }
-
-    return checkpointInputDate;
-  }
 
  
   render() {
 
   
     return (
-      <div className="appcenter">
-        <label>Start Date</label>
-        <DatePicker
-          selected={this.state.startDate}
-          onChange={this.handleChange}
-          filterDate={this.isWeekday}
-        />
-       
-
-
-                <br/>
-
-       <label>checkpoint1</label><br/>
-        <input type="text"
-          value={this.state.checkpoint[0]}
-        />
-        <br/>
-         <label>checkpoint2</label><br/>
-        <input type="text"
-          value={this.state.checkpoint[1]}
-        />
-         <br/>
-        <label>checkpoint3</label><br/>
-        <input type="text"
-          value={this.state.checkpoint[2]}
-        />
-         <br/>
-         <label>Mid Date</label>
-        <DatePicker
-          selected={this.state.midDate}
-          onChange={this.handle_midDate}
-          filterDate={this.isWeekday}
-        />
-             <br/>
-        <button onClick={this.diffTest}>Save</button>
+      <div>
+  <DatePickerInput />
       </div>
     );
   }
